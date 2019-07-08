44831b08ae786fecc06e52e5a005b673b63cdeab
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-08 21:44:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,12 @@
 def nyc_pigeon_organizer(data)
   new_hash ={}
   data.each {|property,hash|}
-  array.each do 
+  array.each do |name| 
+    
+    if new_hash == nil 
+      new_hash[name] = {}
+    end 
+    
+    
+    
 end
